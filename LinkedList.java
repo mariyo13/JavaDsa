65f4b4c59044d61331a1c3ad68edc560c1b9f0b2
mariyo <agashmanu@gmail.com>
2024-11-12 20:21:57 +0530

    node is created

---
+++ LinkedList.java
@@ -0,0 +1,12 @@
+public class LinkedList {
+
+    private class Node {
+        private int data;
+        Node nexNode;
+
+        public Node(int data) {
+            this.data = data;
+            this.nexNode = null;
+        }
+    }
+}
