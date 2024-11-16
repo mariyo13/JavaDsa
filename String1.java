cd6718be926e2e381b82fd0374268d45bf013fb5
mariyo <agashmanu@gmail.com>
2024-11-16 19:45:37 +0530

    isAnagram

--- String1.java
+++ String1.java
@@ -34,9 +34,32 @@ public class String1 {
         return subString.length() == j;
     }
 
+    // is anagram
+    public static boolean isAnagram(String s1, String s2) {
+        if (s1.length() == 0 || s2.length() == 0) {
+            return false;
+        }
+        if (s1.length() != s2.length()) {
+            return false;
+        }
+        int[] num = new int[256];
+        for (int i = 0; i < s1.length(); i++) {
+            num[s1.charAt(i)]++;
+            num[s2.charAt(i)]--;
+        }
+        for (int i = 0; i < num.length; i++) {
+            if (num[i] != 0) {
+                return false;
+            }
+        }
+        return true;
+
+    }
+
     public static void main(String[] args) {
         System.out.println(isPolindrem("manu"));
         System.out.println(isPolindrem("malalam"));
         System.out.println(isSubequence("greeksforgreeks", "grf"));
+        System.out.println(isAnagram("manu", "unam"));
     }
 }
