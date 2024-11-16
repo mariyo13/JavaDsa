ba9d6c449820b35f5bdd2e49f76d644a43f2ce4c
mariyo <agashmanu@gmail.com>
2024-11-16 19:31:28 +0530

    functionality of isPolindrem and isSubsquence is added

--- String1.java
+++ String1.java
@@ -1,5 +1,42 @@
-public class String1{
+public class String1 {
+    // to check wheather given String is polindrem are not
+    public static boolean isPolindrem(String str) {
+        if (str == null) {
+            return false;
+        }
+        int start = 0;
+        int end = str.length() - 1;
+
+        while (start < end) {
+            if (str.charAt(start) != str.charAt(end)) {
+                return false;
+            }
+            start++;
+            end--;
+        }
+        return true;
+    }
+
+    // to subsequence are not
+    public static boolean isSubequence(String string, String subString) {
+        if (string == null) {
+            return false;
+        }
+        int i = 0, j = 0;
+
+        while (i < string.length() && j < subString.length()) {
+            if (string.charAt(i) == subString.charAt(j)) {
+                j++;
+            }
+            i++;
+        }
+        System.out.println(i + subString.length());
+        return subString.length() == j;
+    }
+
     public static void main(String[] args) {
-        System.out.println("dklsfn");
+        System.out.println(isPolindrem("manu"));
+        System.out.println(isPolindrem("malalam"));
+        System.out.println(isSubequence("greeksforgreeks", "grf"));
     }
 }
