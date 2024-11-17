2b862134fa379af81ca678b01804f181576bd955
mariyo <agashmanu@gmail.com>
2024-11-17 21:34:05 +0530

    pattern2 method is added

--- Pattern.java
+++ Pattern.java
@@ -8,7 +8,16 @@ public class Pattern {
         }
     }
 
+    public static void pattern2(int n) {
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n - i + 1; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+
     public static void main(String[] args) {
-        pattern1(5);
+        pattern2(5);
     }
 }
