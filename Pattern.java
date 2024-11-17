90218edb600b5ef51226bce6554486e679490a1b
mariyo <agashmanu@gmail.com>
2024-11-17 21:38:02 +0530

    another method is added

--- Pattern.java
+++ Pattern.java
@@ -8,6 +8,24 @@ public class Pattern {
         }
     }
 
+    public static void pattern3(int n) {
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(i + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void pattern4(int n) {
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(j + " ");
+            }
+            System.out.println();
+        }
+    }
+
     public static void pattern2(int n) {
         for (int i = 1; i <= n; i++) {
             for (int j = 1; j <= n - i + 1; j++) {
@@ -18,6 +36,6 @@ public class Pattern {
     }
 
     public static void main(String[] args) {
-        pattern2(5);
+        pattern4(5);
     }
 }
