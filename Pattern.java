d9fb6cae66487a7d2c7a8ce8f68e6023c8d2c633
mariyo <agashmanu@gmail.com>
2024-11-18 21:53:54 +0530

    added

--- Pattern.java
+++ Pattern.java
@@ -47,7 +47,52 @@ public class Pattern {
         }
     }
 
+    public static void pattern6(int n) {
+        int k = 1;
+        int alpha = (int) 'a';
+        alpha -= 1;
+        for (int i = 1; i <= n; i++) {
+            ;
+            for (int j = 1; j <= i; j++) {
+                int temp = (alpha + k);
+                System.out.print((char) temp + " ");
+                k++;
+            }
+            System.out.println();
+        }
+    }
+
+    public static void pattern7(int n) {
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print("  ");
+            }
+            for (int j = 5; j >= n + 1 - i; j--) {
+                System.out.print(j + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void pattern8(int n) {
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print("_" + " ");
+            }
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*" + " ");
+            }
+            for (int j = 1; j <= i - 1; j++) {
+                System.out.print("*" + " ");
+            }
+
+            System.out.println();
+        }
+
+    }
+
     public static void main(String[] args) {
-        pattern5(5);
+        pattern8(5);
     }
 }
