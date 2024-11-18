e32896a41fd45a375b27f0a324d61ff2a621e4ce
mariyo <agashmanu@gmail.com>
2024-11-18 21:58:46 +0530

    add

--- Pattern.java
+++ Pattern.java
@@ -92,7 +92,24 @@ public class Pattern {
 
     }
 
+    public static void pattern9(int n) {
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print("_" + " ");
+            }
+            for (int j = 1; j <= i; j++) {
+                System.out.print(j + " ");
+            }
+            for (int j = i - 1; j >= 1; j--) {
+                System.out.print(j + " ");
+            }
+
+            System.out.println();
+        }
+
+    }
+
     public static void main(String[] args) {
-        pattern8(5);
+        pattern9(5);
     }
 }
