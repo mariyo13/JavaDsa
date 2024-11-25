ab03631517a11a860e559350c159faabdd837e1e
mariyo <agashmanu@gmail.com>
2024-11-25 18:29:08 +0530

    add

--- mythread.java
+++ mythread.java
@@ -1,40 +1,42 @@
-class mythread extends Thread {
+// class mythread extends Thread {
 
-    @Override
-    public void run() {
+//     @Override
+//     public void run() {
 
-        for (int i = 0; i < 1; i++) {
-            try {
-                Thread.sleep(2000);
-                System.out.println("the sub thread is running");
-            } catch (InterruptedException ex) {
-            }
+//         for (int i = 0; i < 1; i++) {
+//             try {
+//                 Thread.sleep(2000);
+//                 System.out.println("the sub thread is running");
+//             } catch (InterruptedException ex) {
+//             }
 
-        }
+//         }
 
-    }
+//     }
 
-    public static void main(String[] args) throws InterruptedException {
-        mythread mythread1 = new mythread();
-        System.out.println(mythread1.getState());
-        mythread1.start();
-        System.out.println(mythread1.getState());
-        for (int i = 0; i < 1; i++) {
-            Thread.sleep(2000);
-            System.out.println("the main thread is running");
-            System.out.println(mythread1.getState());
-        }
-        System.out.println(mythread1.getState());
-    }
+//     // public static void main(String[] args) throws InterruptedException {
+//     // mythread mythread1 = new mythread();
+//     // System.out.println(mythread1.getState());
+//     // mythread1.start();
+//     // System.out.println(mythread1.getState());
+//     // for (int i = 0; i < 1; i++) {
+//     // Thread.sleep(2000);
+//     // System.out.println("the main thread is running");
+//     // System.out.println(mythread1.getState());
+//     // }
+//     // System.out.println(mythread1.getState());
+//     // }
 
-}
+// }
 
 // second way of implementing the thread
 class Mythread implements Runnable {
 
     @Override
     public void run() {
-
+        for (int i = 0; i <= 10; i++) {
+            System.out.println("sub thread ");
+        }
     }
 
     public static void main(String[] args) throws InterruptedException {
@@ -42,10 +44,11 @@ class Mythread implements Runnable {
         Thread t1 = new Thread(m);
         t1.start();
         System.out.println(t1.getState());
-        for (int i = 0; i < 100; i++) {
-            Thread.sleep(2000);
+        for (int i = 0; i < 10; i++) {
+
             System.out.println("the main thread is running");
             System.out.println(t1.getState());
+            t1.join();
         }
         System.out.println(t1.getState());
 
