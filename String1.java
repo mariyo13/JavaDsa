e9232126c6482cc81c7d8a668e3f14b6559c6f10
mariyo <agashmanu@gmail.com>
2024-11-25 17:38:17 +0530

    added

--- String1.java
+++ String1.java
@@ -79,7 +79,27 @@ public class String1 {
         return ans;
     }
 
+    // overloaded reverse String methods
+    public static String revseString(String str, int start, int end) {
+        char[] ch = str.toCharArray();
+        int i = start;
+        int j = end;
+        while (i < j) {
+            swap(ch, i, j);
+            i++;
+            j--;
+        }
+        return new String(ch);
+    }
+
+    public static void swap(char[] s, int i, int j) {
+        char temp = s[i];
+        s[i] = s[j];
+        s[j] = temp;
+
+    }
+
     public static void main(String[] args) {
-        System.out.println(revseString("i love yamuna"));
+        System.out.println(revseString("yamuna", 0, 5));
     }
 }
